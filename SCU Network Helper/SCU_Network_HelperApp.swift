21132a5d51631960648a3e5b22dcdf17d9128933
Terminal_Void <xingzhili2007@163.com>
2026-03-15 21:24:37 +0800

    Initial Commit

---
+++ SCU Network Helper/SCU_Network_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCU_Network_HelperApp.swift
+//  SCU Network Helper
+//
+//  Created by Terminal Void on 2026/3/15.
+//
+
+import SwiftUI
+
+@main
+struct SCU_Network_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
